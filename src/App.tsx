0b9a10178fcc2b6bfb0b41d966c5d121dd640db3
hoangthach1402 <vuhoangthach1402@gmail.com>
2023-08-05 10:40:08 +0700

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link,useHistory  } from 'react-router-dom';
+
+import { BrowserRouter as Router, Routes, Route,   } from 'react-router-dom';
 import Home from './page/Home';
 import About from './page/About';
 import Navbar from './components/Navbar';
